8037144e6ad55cf70b960cc4db3fa6a7caa5f312
Julien Dehos <dehos@univ-littoral.fr>
2019-08-05 18:08:45 +0200

    audio if touch bob

--- src/Main.hs
+++ src/Main.hs
@@ -15,13 +15,14 @@ data Model = Model
     } deriving (Eq)
 
 data Action
-    = AddOne
-    | NoOp
+    = ActionInc
+    | ActionNone
+    | ActionPlay
     deriving (Show, Eq)
 
 main :: IO ()
 main = startApp App
-    { initialAction = NoOp
+    { initialAction = ActionNone
     , model  = Model 0
     , update = updateModel
     , view   = viewModel
@@ -31,14 +32,25 @@ main = startApp App
     }
 
 updateModel :: Action -> Model -> Effect Action Model
-updateModel AddOne m = noEff m { _val = 1 + _val m }
-updateModel NoOp m = noEff m
+updateModel ActionInc m = noEff m { _val = 1 + _val m }
+updateModel ActionNone m = noEff m
+updateModel ActionPlay m = m <# do
+    jsPlay
+    pure ActionNone
 
 viewModel :: Model -> View Action
 viewModel m = div_ []
     [ h1_ [] [ text "miso-invaders" ]
-    , p_ [] [ text (ms $ _val m), button_ [ onClick AddOne ] [ text "+" ] ]
-    , p_ [] [ img_ [ src_ "spongebob-small.png" ] ]
-    , p_ [] [ audio_ [ src_ "47.mp3", controls_ True ] [] ]
+    , p_ [] [ audio_ [ id_ "myaudio", src_ "47.mp3", controls_ False ] [] ]
+    , p_ [] [ text "Touch Bob to play a sound !" ]
+    , p_ [] [ img_ [ src_ "spongebob-small.png", onClick ActionPlay ] ]
+    , p_ [] 
+         [ a_ [ href_ "https://gitlab.com/juliendehos/miso-invaders"] [ text "source code" ]
+         , text " / "
+         , a_ [ href_ "https://juliendehos.gitlab.io/miso-invaders"] [ text "demo" ]
+         ]
     ]
+
+foreign import javascript unsafe "myaudio.play();"
+    jsPlay :: IO ()
 
