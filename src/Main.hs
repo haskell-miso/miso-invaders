d18efce9920412fd59bfa4fd7e56df28518407ba
Julien Dehos <dehos@univ-littoral.fr>
2019-08-05 18:34:24 +0200

    canvas

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Map (singleton)
+-- import JavaScript.Web.Canvas
 import Miso
-import Miso.String
-
--- TODO test animation
--- TODO implement game
+import Miso.String hiding (singleton)
 
 -- https://github.com/Lermex/miso-plane/blob/master/src/Update.hs
--- http://hackage.haskell.org/package/miso-1.2.0.0/docs/Miso-Html-Element.html
--- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+-- https://github.com/dmjio/miso/blob/master/examples/canvas2d/Main.hs
 
 data Model = Model
     { _val :: Int
     } deriving (Eq)
 
 data Action
-    = ActionInc
-    | ActionNone
+    = ActionNone
     | ActionPlay
     deriving (Show, Eq)
 
@@ -32,7 +29,6 @@ main = startApp App
     }
 
 updateModel :: Action -> Model -> Effect Action Model
-updateModel ActionInc m = noEff m { _val = 1 + _val m }
 updateModel ActionNone m = noEff m
 updateModel ActionPlay m = m <# do
     jsPlay
@@ -44,6 +40,10 @@ viewModel m = div_ []
     , p_ [] [ audio_ [ id_ "myaudio", src_ "47.mp3", controls_ False ] [] ]
     , p_ [] [ text "Touch Bob to play a sound !" ]
     , p_ [] [ img_ [ src_ "spongebob-small.png", onClick ActionPlay ] ]
+    , p_ [] [ canvas_ [ id_ "mycanvas" , width_ "600" , height_ "400"
+                      , style_  (singleton "border" "1px solid black")
+                      , onMouseDown ActionPlay ] [] ]
+
     , p_ [] 
          [ a_ [ href_ "https://gitlab.com/juliendehos/miso-invaders"] [ text "source code" ]
          , text " / "
