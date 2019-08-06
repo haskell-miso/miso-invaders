62a1ab82157626e5fb81092ce2ec71b494496e3c
Julien Dehos <dehos@univ-littoral.fr>
2019-08-06 11:45:18 +0200

    proto3

--- proto/proto3.hs
+++ proto/proto3.hs
@@ -10,7 +10,7 @@ type Model = Double
 data Action
   = NoOp
   | GetTime
-  | SetTime Model
+  | SetTime
 
 main :: IO ()
 main = do
@@ -22,7 +22,7 @@ main = do
     , view   = \_ -> canvas_ [ id_ "canvas" , width_ "300" , height_ "300"
                              , style_  (singleton "border" "1px solid black")
                              ] []
-    , model  = 0.0
+    , model  = 100
     , subs   = []
     , events = defaultEvents
     , mountPoint = Nothing
@@ -30,18 +30,12 @@ main = do
 
 updateModel :: Image -> Action -> Model -> Effect Action Model
 updateModel _ NoOp m = noEff m
-updateModel _ GetTime m = m' <# do
-  pure $ SetTime m'
-  where m' = if m > 300 then 0 else m + 1
-updateModel earth (SetTime m) _ = m <# do
+updateModel _ GetTime m = (if m > 200 then 100 else m + 1) <# pure SetTime
+updateModel earth SetTime m = m <# do
   ctx <- getCtx
-  setGlobalCompositeOperation ctx
   clearRect 0 0 300 300 ctx
   drawImageXY earth m 200 ctx
   pure GetTime
-
-foreign import javascript unsafe "$1.globalCompositeOperation = 'destination-over';"
-  setGlobalCompositeOperation :: Context -> IO ()
 
 foreign import javascript unsafe "$4.drawImage($1,$2,$3);"
   drawImageXY :: Image -> Double -> Double -> Context -> IO ()
