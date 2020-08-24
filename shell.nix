87db847c097df82fa07b9df5efc17f681179d7e5
Julien Dehos <dehos@univ-littoral.fr>
2020-08-24 10:25:56 +0200

    up

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
 with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/39b9e26ff41d6aab3b9d13a9d102ac56017f6a1f.tar.gz";
-  sha256 = "1lwr35p9074b7wgz0jh4f2pjc7ls8isgzmn9xl86vb6cvsm035kf";
+  url = "https://github.com/dmjio/miso/archive/561ffad.tar.gz";
+  sha256 = "1wwzckz2qxb873wdkwqmx9gmh0wshcdxi7gjwkba0q51jnkfdi41";
 }) {});
-(pkgs.haskell.packages.ghcjs.callCabal2nix "miso-invaders" ./. {}).env
-
+pkgs.haskell.packages.ghcjs.callCabal2nix "miso-invaders" ./. {}
