93d92da97c8e8fc43191070af926a24ff11b2786
Julien Dehos <dehos@univ-littoral.fr>
2025-05-16 12:00:35 +0200

    display canvas

--- src/Main.hs
+++ src/Main.hs
@@ -8,10 +8,11 @@ module Main where
 -- import Language.Javascript.JSaddle (JSM, (#), fromJSVal, new, jsg)
 
 import Miso
-import Miso.Canvas
-import qualified Miso.Canvas as Canvas
--- import Miso.String
-import Miso.Style
+import Miso.Canvas as Canvas
+import Miso.String qualified as String
+import Miso.Style as Style
+
+import Game
 
 #ifdef WASM
 foreign export javascript "hs_start" main :: IO ()
@@ -35,20 +36,29 @@ main =
 handleView :: Image -> Model -> View Action
 handleView sb model = 
   div_ [] 
-    [ p_ [] ["foobar"]
-    -- , div_ 
-    --     [ id_ "canvas grid" ] 
-    --     [ Canvas.canvas_ 
-    --       [ id_ "canvas", width_ "800", height_ "600" ] (canvasDraw sb model)
-    --     ]
-    ]
+    [ p_ [] [ "Usage: left/right to move, space to fire and enter to start..." ]
+    , Canvas.canvas_ 
+        [ id_ "mycanvas"
+        , width_ (String.ms gameWidth)
+        , height_ (String.ms gameHeight)
+        , Style.style_  [Style.border "1px solid black"]
+        ] 
+        (canvasDraw sb model)
+    , p_ []
+         [ a_ [ href_ "https://gitlab.com/juliendehos/miso-invaders"]
+              [ text "source code" ]
+         , text " / "
+         , a_ [ href_ "https://juliendehos.gitlab.io/miso-invaders"]
+              [ text "demo" ]
+         ]
+ ]
 
 canvasDraw :: Image -> Model -> Canvas ()
 canvasDraw sb () = do
    globalCompositeOperation DestinationOver
    clearRect (0, 0, 800, 600)
-   fillStyle $ Canvas.color (rgba 0 0 0 0.6)
-   strokeStyle $ Canvas.color (rgba 0 153 255 0.4)
+   fillStyle $ Canvas.color (Style.rgba 0 0 0 0.6)
+   strokeStyle $ Canvas.color (Style.rgba 0 153 255 0.4)
    drawImage' (sb, 0, 0, 50, 50)
 
 handleUpdate :: Action -> Effect Model Action
