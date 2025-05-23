b145804c3b1f877f579924b97c02ecd970c101b6
Julien Dehos <dehos@univ-littoral.fr>
2025-05-23 11:10:11 +0200

    fps

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE StrictData #-}
 
-import Control.Monad (void)
+import Control.Monad
 import Data.Set qualified as S
 import Language.Javascript.JSaddle (JSM, jsg, (#))
 import Control.Lens hiding ((#), view)
@@ -25,6 +24,11 @@ paddleHeight = 66
 paddleImgName :: MS.MisoString
 paddleImgName = "spongebob.png"
 
+idAudioTouched, idAudioWon, idAudioLost :: MS.MisoString
+idAudioTouched = "myAudioTouched" 
+idAudioWon = "myAudioWon" 
+idAudioLost = "myAudioLost" 
+
 ----------------------------------------------------------------------
 -- types
 ----------------------------------------------------------------------
@@ -32,6 +36,9 @@ paddleImgName = "spongebob.png"
 data Model = Model
   { _mGame :: Game
   , _mTime :: Double
+  , _mFps :: Int
+  , _mFpsTime :: Double
+  , _mFpsTicks :: Int
   } deriving (Eq)
 
 data Action 
@@ -48,11 +55,20 @@ data Action
 makeLenses ''Model
 -}
 
+mGame :: Lens' Model Game
+mGame f o = (\x' -> o {_mGame = x'}) <$> f (_mGame o)
+
 mTime :: Lens' Model Double
 mTime f o = (\x' -> o {_mTime = x'}) <$> f (_mTime o)
 
-mGame :: Lens' Model Game
-mGame f o = (\x' -> o {_mGame = x'}) <$> f (_mGame o)
+mFps :: Lens' Model Int
+mFps f o = (\x' -> o {_mFps = x'}) <$> f (_mFps o)
+
+mFpsTime :: Lens' Model Double
+mFpsTime f o = (\x' -> o {_mFpsTime = x'}) <$> f (_mFpsTime o)
+
+mFpsTicks :: Lens' Model Int
+mFpsTicks f o = (\x' -> o {_mFpsTicks = x'}) <$> f (_mFpsTicks o)
 
 ----------------------------------------------------------------------
 -- view handler
@@ -68,6 +84,7 @@ handleView paddleImg model = div_ []
       , Style.style_  [Style.border "1px solid black"]
       ] 
       (canvasDraw paddleImg model)
+  , p_ [] [ text ("fps: " <> MS.ms (model^.mFps)) ] 
   , p_ []
        [ a_ [ href_ "https://gitlab.com/juliendehos/miso-invaders"]
             [ text "source code" ]
@@ -76,9 +93,9 @@ handleView paddleImg model = div_ []
             [ text "demo" ]
        ]
   , p_ [] 
-      [ audio_ [ id_ "myAudioTouched", src_ "touched.mp3" ] []
-      , audio_ [ id_ "myAudioWon", src_ "won.mp3" ] []
-      , audio_ [ id_ "myAudioLost", src_ "lost.mp3" ] []
+      [ audio_ [ id_ idAudioTouched, src_ "touched.mp3" ] []
+      , audio_ [ id_ idAudioWon, src_ "won.mp3" ] []
+      , audio_ [ id_ idAudioLost, src_ "lost.mp3" ] []
       ]
   ]
 
@@ -123,32 +140,41 @@ handleUpdate ActionReset = do
   io (ActionStep <$> myGetTime)
 
 handleUpdate (ActionKey keys) = 
-  if S.member 13 keys
+  if S.member 13 keys   -- Enter
   then issue ActionReset
   else do
-    mGame . inputLeft  .= S.member 37 keys
-    mGame . inputRight .= S.member 39 keys
-    mGame . inputFire  .= S.member 32 keys
+    mGame . inputLeft  .= S.member 37 keys    -- Left arrow
+    mGame . inputRight .= S.member 39 keys    -- Right arrow
+    mGame . inputFire  .= S.member 32 keys    -- Space
 
 handleUpdate (ActionStep t1) = do
   t0 <- use mTime
-  mGame %= step (t1 - t0)
+  let dt = t1 - t0
+  mGame %= step dt
   mTime .= t1
   touched <- uses mGame _hasTouched
   st <- uses mGame _status
   case (st, touched) of
-    (Won, _) -> io_ $ jsPlayAudio "myAudioWon"
-    (Lost, _) -> io_ $ jsPlayAudio "myAudioLost"
+    (Won, _) -> io_ $ jsPlayAudio idAudioWon
+    (Lost, _) -> io_ $ jsPlayAudio idAudioLost
     (Running, False) -> io (ActionStep <$> myGetTime)
-    (Running, True) -> io (jsPlayAudio "myAudioTouched" >> ActionStep <$> myGetTime)
+    (Running, True) -> io (jsPlayAudio idAudioTouched >> ActionStep <$> myGetTime)
     _ -> pure ()
+  mFpsTime += dt
+  mFpsTicks += 1
+  fpsTime <- use mFpsTime
+  when (fpsTime > 1) $ do
+    ticks <- use mFpsTicks
+    mFps .= ticks
+    mFpsTime .= 0
+    mFpsTicks .= 0
 
 ----------------------------------------------------------------------
 -- JavaScript FFI
 ----------------------------------------------------------------------
 
-jsPlayAudio :: String -> JSM ()
-jsPlayAudio name = void $ jsg name # ("play"::String) $ ()
+jsPlayAudio :: MS.MisoString -> JSM ()
+jsPlayAudio name = void $ jsg name # ("play"::MS.MisoString) $ ()
 
 ----------------------------------------------------------------------
 -- main
@@ -160,11 +186,10 @@ myGetTime = (* 0.001) <$> now
 main :: IO ()
 main = run $ do
   paddleImg <- newImage paddleImgName
-  time <- myGetTime
-  game <- mkGame paddleWidth paddleHeight . take 1000 . randoms <$> newStdGen
-  -- let game = mkGame paddleWidth paddleHeight []
+  myRands <- take 1000 . randoms <$> newStdGen
+  let game = mkGame paddleWidth paddleHeight myRands
   startComponent Component
-    { model = Model game time
+    { model = Model game 0 0 0 0
     , update = handleUpdate
     , view = handleView paddleImg
     , subs = [ keyboardSub ActionKey ]
